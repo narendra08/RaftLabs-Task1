22ec4f782cd19130a54f9476d009ccb22af5991c
Narendra Singh <narendrapratap.98@gmail.com>
2023-03-14 08:22:58 +0530

    auth middleware refactored

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,16 @@
 const jwt = require("jsonwebtoken");
 
 function getToken() {
-  const token = jwt.sign("admin", "MY_SECRET");
+  const token = jwt.sign("admin", "MY_SECRET", { expiresIn: 3600 });
   return token;
 }
 function verifyToken(req, res, next) {
-  const  token  = req.headers.authorization;
+  const token = req.headers.authorization;
   if (!token) {
     res.status(403).json({ msg: "no token " });
   }
   try {
-    const decoded = jwt.verify(token, "MY_SECRET");
-    // req.user = decoded;
+    jwt.verify(token, "MY_SECRET");
   } catch (err) {
     return res.json({
       msg: "Invalid Token",
